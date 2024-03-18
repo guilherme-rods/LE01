97e1ffd25879285ba5b408345f0a32cd15667f4e
Guilherme Rodrigues <guilherme.g.rods@gmail.com>
2024-03-17 21:19:28 -0300

    commit Ex05

---
+++ Ex05.java
@@ -0,0 +1,36 @@
+//5. Leia valores nas variáveis A e B, e efetue a troca dos valores de forma que o valor da variável A 
+//passe a ser o valor da variável B e o valor da variável B passe a ser o valor da variável A. 
+//Apresentar uma mensagem com o valor original de cada variável e outra com os valores trocados.
+
+import java.util.Scanner;
+
+public class Ex05 {
+
+    public static void executar(String[] args) {
+
+        System.out.println("Exercício 05:");
+        
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite o valor da variável A: ");
+        double A = leitor.nextDouble();
+        
+        System.out.println("Digite o valor da variável B: ");
+        double B = leitor.nextDouble();
+
+        System.out.println("Valor original de A : " + A);
+        System.out.println("Valor original de B: " + B);
+
+        double aux1 = 0;
+
+        aux1 = A;
+        A = B;
+        B = aux1;
+        
+        System.out.println("Valor trocado de A: " + A);
+        System.out.println("Valor trocado de B: " + B);
+
+        leitor.close();
+    }
+
+}
--- Programa.java
+++ Programa.java
@@ -6,7 +6,7 @@ public class Programa {
         Ex02.executar(args);
         Ex03.executar(args);
         Ex04.executar(args);
-        
+        Ex05.executar(args);
         
     }
 
