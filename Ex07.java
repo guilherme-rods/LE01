7eae69dc70d774b869ccf23128001c0c3fec3045
Guilherme Rodrigues <guilherme.g.rods@gmail.com>
2024-03-17 21:29:52 -0300

    commit Ex07

---
+++ Ex07.java
@@ -0,0 +1,29 @@
+//7. Escreva um algoritmo que leia um número e diga, através de uma mensagem, se este número está no intervalo entre 100 e 200. 
+//Caso o número esteja fora do intervalo o usuário também deverá ser informado.
+
+import java.util.Scanner;
+
+public class Ex07 {
+
+    public static void executar(String[] args) {
+        
+        System.out.println("Exercício 07: ");
+
+        Scanner leitor = new Scanner(System.in);
+        
+        System.out.println("Digite um número entre 100 e 200: ");
+        int num = leitor.nextInt();
+
+        if(100 <= num && num <= 200){
+
+            System.out.println("O número escolhido está dentro do intervalo determinado!");
+
+        }else{
+
+            System.out.println("O número escolhido está fora do intervalor determinado!");
+        }
+
+        leitor.close();
+    }
+
+}
--- Programa.java
+++ Programa.java
@@ -2,12 +2,15 @@ public class Programa {
 
     public static void main(String[] args) {
 
+        System.out.println("LE01 - Guilherme Rodrigues:");
+
         Ex01.executar(args);
         Ex02.executar(args);
         Ex03.executar(args);
         Ex04.executar(args);
         Ex05.executar(args);
         Ex06.executar(args);
+        Ex07.executar(args);
         
     }
 
